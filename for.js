346de44b4c50e54310b51252becce31b0a341f3f
thomasnu45 <thomyezequiel07052004@gmail.com>
2025-03-21 09:10:31 -0300

    Add files via upload

---
+++ for.js
@@ -0,0 +1,227 @@
+
+var cantidadDeGatos = 10;
+
+for (var i = 1; i <= cantidadDeGatos; i++) {
+    var emoji = i % 3 === 1 ? '😺' : i % 3 === 2 ? '😸' : '😹';
+    console.log(`Gato #${i}: ${emoji}`);
+}
+
+var cantidadDePasos = 4;
+
+for (var i = 1; i <= cantidadDeGatos; i++) {
+  console.log(`Gato #${i}: 🐈 ${'🐾'.repeat(cantidadDePasos)}`);
+}
+
+
+
+for (var i = 1; i <= cantidadDeGatos; i++) {
+  console.log(`Gato #${i}: 🐈 ${'🐾'.repeat(cantidadDePasos)}`);
+  
+  if (i % 2 === 0) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+}
+
+
+for (var i = 1; i <= cantidadDeGatos; i++) {
+  console.log(`Gato #${i}: 🐈 ${'🐾'.repeat(cantidadDePasos)}`);
+  
+  if (i % 2 === 0) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i % 2 !== 0 && i < cantidadDeGatos) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+}
+
+
+for (var i = 1; i <= cantidadDeGatos; i++) {
+  console.log(`Gato #${i}: 🐈 ${'🐾'.repeat(cantidadDePasos)}`);
+  
+  if (i % 2 === 0) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i % 2 !== 0 && i < cantidadDeGatos) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i === cantidadDeGatos) {
+    break;
+  }
+}
+
+
+
+for (var i = 1; i <= cantidadDeGatos; i++) {
+  console.log(`Gato #${i}: 🐈 ${'🐾'.repeat(cantidadDePasos)}`);
+  
+  if (i % 2 === 0) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i % 2 !== 0 && i < cantidadDeGatos) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i === cantidadDeGatos) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 1) {
+    break;
+  }
+}
+
+
+for (var i = 1; i <= cantidadDeGatos; i++) {
+  console.log(`Gato #${i}: 🐈 ${'🐾'.repeat(cantidadDePasos)}`);
+  
+  if (i % 2 === 0) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i % 2 !== 0 && i < cantidadDeGatos) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i === cantidadDeGatos) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 1) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 2) {
+    break;
+  }
+}
+
+for (var i = 1; i <= cantidadDeGatos; i++) {
+  console.log(`Gato #${i}: 🐈 ${'🐾'.repeat(cantidadDePasos)}`);
+  
+  if (i % 2 === 0) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i % 2 !== 0 && i < cantidadDeGatos) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i === cantidadDeGatos) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 1) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 2) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 3) {
+    break;
+  }
+}
+
+
+
+for (var i = 1; i <= cantidadDeGatos; i++) {
+  console.log(`Gato #${i}: 🐈 ${'🐾'.repeat(cantidadDePasos)}`);
+  
+  if (i % 2 === 0) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i % 2 !== 0 && i < cantidadDeGatos) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i === cantidadDeGatos) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 1) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 2) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 3) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 4) {
+    break;
+  }
+}
+
+for (var i = 1; i <= cantidadDeGatos; i++) {
+  console.log(`Gato #${i}: 🐈 ${'🐾'.repeat(cantidadDePasos)}`);
+  
+  if (i % 2 === 0) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i % 2 !== 0 && i < cantidadDeGatos) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i === cantidadDeGatos) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 1) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 2) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 3) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 4) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 5) {
+    break;
+  }
+}
+
+
+for (var i = 1; i <= cantidadDeGatos; i++) {
+  console.log(`Gato #${i}: 🐈 ${'🐾'.repeat(cantidadDePasos)}`);
+  
+  if (i % 2 === 0) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i % 2 !== 0 && i < cantidadDeGatos) {
+    console.log(`Gato #${i + 1}: 🐈`);
+  }
+  
+  if (i === cantidadDeGatos) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 1) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 2) {
+    break;
+  }
+  
+  if (i === cantidadDeGatos - 3) {
+    break;
+  }
+  }
